3b8ea189eefb2f34f756724d59fb03fdcbc73aaa
Esteban de la Torre <edelatorre94@gmail.com>
2023-12-05 20:33:24 -0800

    2.33 Create event when comments are created

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -10,14 +10,14 @@ app.post('/events', (req, res) => {
   const event = req.body;
 
   axios.post('http://localhost:4000/events', event).catch((e) => {
-    console.log(e);
+    console.log(e.message);
   });
   axios.post('http://localhost:4001/events', event).catch((e) => {
-    console.log(e);
+    console.log(e.message);
   });
-  //   axios.post('http://localhost:4002/events', event).catch((e) => {
-  //     console.log(e);
-  //   });
+  axios.post('http://localhost:4002/events', event).catch((e) => {
+    console.log(e.message);
+  });
 
   res.send({ status: 'OK' });
 });
