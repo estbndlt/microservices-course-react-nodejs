97140d49ad27f90595f9dabcbbcb12e9d09d1640
Esteban de la Torre <edelatorre94@gmail.com>
2023-12-05 20:28:08 -0800

    2.32 Add event bus and emit events to posts and comments service

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,6 +24,10 @@ app.post('/posts/:id/comments', (req, res) => {
   commentsByPostId[req.params.id] = comments;
 
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('got an event');
 });
 
 app.listen(4001, () => {
