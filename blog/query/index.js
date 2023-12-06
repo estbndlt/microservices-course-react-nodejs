dc2cb6ab923274f82aed0b897a4719cd784cbbdb
Esteban de la Torre <edelatorre94@gmail.com>
2023-12-05 21:57:11 -0800

    2.47 Add ability to moderate contents (lots of events)

--- blog/query/index.js
+++ blog/query/index.js
@@ -22,14 +22,26 @@ app.post('/events', (req, res) => {
   }
 
   if (type === 'CommentCreated') {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
 
-  console.log('got an event', req.body.type);
+  if (type === 'CommentUpdated') {
+    const { id, status, postId, content } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
   console.log(posts);
+  console.log('got an event', req.body.type);
   res.send({});
 });
 
