8d542090fb21c6d5cd1412c16af048cbaed7c9b0
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-02 17:34:13 -0800

    9.184 Add middleware for validating requests

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middleware/validate-request';
 
 const router = express.Router();
 
@@ -11,15 +11,8 @@ router.post(
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().notEmpty().withMessage('Password must be provided'),
   ],
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
-    const { email, password } = req.body;
-  }
+  validateRequest,
+  async (req: Request, res: Response) => {}
 );
 
 export { router as signinRouter };
