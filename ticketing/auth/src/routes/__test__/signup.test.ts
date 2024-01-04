eb3384111c8ba125fdaf13a3f52c6ab75c939ff3
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-03 21:35:04 -0800

    10.205 Change node environment for test

--- ticketing/auth/src/routes/__test__/signup.test.ts
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -1,26 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-it('returns a 201 on successful signup', async () => {
-  return request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201);
-});
-
-it('returns a 400 with an invalid email', async () => {
-  return request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'testtest',
-      password: 'password',
-    })
-    .expect(400);
-});
-
 it.each`
   payload                                                 | expectedStatusCode | description
   ${{ email: 'valid@example.com', password: 'P@ssw0rd' }} | ${201}             | ${'valid payload'}
@@ -37,3 +17,33 @@ it.each`
       .expect(expectedStatusCode);
   }
 );
+
+it('returns 400 with account already created', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('sets a cookie on successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
