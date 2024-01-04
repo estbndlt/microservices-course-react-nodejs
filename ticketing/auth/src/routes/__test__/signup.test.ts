f798f5f9582e5216ef67fd72ec22e44bd1112a7c
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-03 21:26:55 -0800

    10.203 Add additional tests using a table

--- ticketing/auth/src/routes/__test__/signup.test.ts
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,30 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it.each`
+  payload                                                 | expectedStatusCode | description
+  ${{ email: 'valid@example.com', password: 'P@ssw0rd' }} | ${201}             | ${'valid payload'}
+  ${{ email: 'invalid', password: 'P@ssw0rd' }}           | ${400}             | ${'invalid email'}
+  ${{ email: 'valid@example.com', password: 'P' }}        | ${400}             | ${'invalid password'}
+  ${{ email: '', password: '' }}                          | ${400}             | ${'invalid payload'}
+  ${{}}                                                   | ${400}             | ${'no payload'}
+`(
+  'return $expectedStatusCode with $description (payload: $payload)',
+  async ({ payload, expectedStatusCode }) => {
+    return request(app)
+      .post('/api/users/signup')
+      .send(payload)
+      .expect(expectedStatusCode);
+  }
+);
