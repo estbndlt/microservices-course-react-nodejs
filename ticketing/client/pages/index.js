fdc855f1aef12503b14ef7e93cb1bff080dbc6aa
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-08 08:27:32 -0800

    11.239 Add server and browser axios requests to get current user

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,5 +1,25 @@
-const Landing = () => {
+import axios from 'axios';
+
+const Landing = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Index</h1>;
+};
+
+Landing.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // on the server
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // on the browser
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
 };
 
 export default Landing;
