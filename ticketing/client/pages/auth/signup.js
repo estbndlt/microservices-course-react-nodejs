d5ea469263a1ca44a65632e24c6018f96fb38baa
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-07 20:47:06 -0800

    11.222 Add Sign Up form components

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -2,6 +2,15 @@ export default () => {
   return (
     <form>
       <h1>Sign up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input className="form-control" />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input type="password" className="form-control" />
+        <button className="btn btn-primary">Sign Up</button>
+      </div>
     </form>
   );
 };
