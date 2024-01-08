b8e59f2d226223a91f6d9e27914da968aed9c462
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-07 21:48:03 -0800

    11.227 Add a hook for request handling

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,21 +1,23 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+  });
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const rsp = await axios.post('/api/users/signup', { email, password });
-      console.log(rsp.data);
-    } catch (e) {
-      console.log(e.response.data);
-      setErrors(e.response.data.errors);
-    }
+    doRequest();
   };
 
   return (
@@ -38,18 +40,9 @@ export default () => {
           type="password"
           className="form-control"
         />
-        {errors.length > 0 && (
-          <div className="alert alert-danger">
-            <h4>Oops...</h4>
-            <ul className="my-0">
-              {errors.map((err) => (
-                <li key={err.message}>{err.message}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-        <button className="btn btn-primary">Sign Up</button>
       </div>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
     </form>
   );
 };
