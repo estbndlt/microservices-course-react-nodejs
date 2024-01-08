9f66b0215da5f8538b989060f4903d41f8930487
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-07 21:33:54 -0800

    11.225 Handle validation errors

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -4,12 +4,18 @@ import axios from 'axios';
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    const rsp = await axios.post('/api/users/signup', { email, password });
-    console.log(rsp.data);
+    try {
+      const rsp = await axios.post('/api/users/signup', { email, password });
+      console.log(rsp.data);
+    } catch (e) {
+      console.log(e.response.data);
+      setErrors(e.response.data.errors);
+    }
   };
 
   return (
@@ -32,6 +38,16 @@ export default () => {
           type="password"
           className="form-control"
         />
+        {errors.length > 0 && (
+          <div className="alert alert-danger">
+            <h4>Oops...</h4>
+            <ul className="my-0">
+              {errors.map((err) => (
+                <li key={err.message}>{err.message}</li>
+              ))}
+            </ul>
+          </div>
+        )}
         <button className="btn btn-primary">Sign Up</button>
       </div>
     </form>
