74f7fb6ca3112921e0c90c0ab9bd36b501a1bd49
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-17 22:24:20 -0800

    16.347 Add mock cleanup and NATS env variables

--- ticketing/tickets/src/routes/__test__/new.test.ts
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 // moved to test/setup.ts so other modules can also use this
 // jest.mock('../../nats-wrapper');
@@ -78,4 +79,24 @@ it('creates a ticket with valid inputs', async () => {
   tickets = await Ticket.find({});
   expect(tickets.length).toEqual(1);
   expect(tickets[0].price).toEqual(10);
+});
+
+it('publishes an event', async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      title: 'valid',
+      price: 10,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(10);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
--- ticketing/tickets/src/routes/__test__/update.test.ts
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 import mongoose from 'mongoose';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -105,4 +106,36 @@ it('updates the ticket if payload is valid', async () => {
 
   expect(ticketRsp.body.title).toEqual('concert');
   expect(ticketRsp.body.price).toEqual(20);
+});
+
+it('publishes an event', async () => {
+  const cookie = global.signup();
+
+  const rsp = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'valid',
+      price: 10,
+    })
+    .expect(201);
+
+  let ticketRsp = await request(app)
+    .put(`/api/tickets/${rsp.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'concert',
+      price: 20,
+    })
+    .expect(200);
+
+  expect(ticketRsp.body.title).toEqual('concert');
+  expect(ticketRsp.body.price).toEqual(20);
+
+  ticketRsp = await request(app).get(`/api/tickets/${rsp.body.id}`).expect(200);
+
+  expect(ticketRsp.body.title).toEqual('concert');
+  expect(ticketRsp.body.price).toEqual(20);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
