7065d425a998c5687ccccc33fc0f54ac8ee152ee
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-21 19:05:41 -0800

    17.358 Add Order model, missing ticket ref

---
+++ ticketing/orders/src/models/order.ts
@@ -0,0 +1,61 @@
+import { OrderStatus } from '@estbndlt-tickets/common';
+import mongoose, { Document, Model } from 'mongoose';
+
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  //   ticket: TicketDoc;
+}
+
+// An interface that describes the properties
+// that a Ticket Document has
+interface OrderDocument extends Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  //   ticket: TicketDoc;
+}
+
+interface OrderModel extends Model<OrderDocument> {
+  build(attrs: OrderAttrs): OrderDocument;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDocument, OrderModel>('Order', orderSchema);
+
+export { Order };
