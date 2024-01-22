4311d1c47a5a48296cdd57d57d8484909055db3e
Esteban de la Torre <edelatorre94@gmail.com>
2024-01-22 09:05:57 -0800

    18.381 Add tests for order events published

--- ticketing/orders/src/routes/__test__/delete.test.ts
+++ ticketing/orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 import { Order, OrderStatus } from '../../models/order';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('marks a ticket as cancelled', async () => {
   const ticket = Ticket.build({
@@ -29,4 +30,29 @@ it('marks a ticket as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo('emits an order cancelled event');
+it('emits an order cancelled event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signup();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
