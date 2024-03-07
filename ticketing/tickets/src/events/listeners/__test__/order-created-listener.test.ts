c91c35a6bdb76484cd76cf1d5ab3cfadf1547c81
Esteban de la Torre <edelatorre94@gmail.com>
2024-03-06 21:57:18 -0800

    19.421 Test order created event

---
+++ ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,40 @@
+import { OrderCreatedEvent, OrderStatus } from '@estbndlt-tickets/common';
+import { Ticket } from '../../../models/ticket';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCreatedListener } from '../order-created-listener';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+
+const setup = async () => {
+  // create instance of listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // create and save a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+    userId: 'asdf',
+  });
+
+  await ticket.save();
+
+  // create the fake data event
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: 'asdf',
+    expiresAt: 'asdf',
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
